314b861689d1f4ee5925da63d5d2f0ae22245048
Daniël <59751559+daniel1890@users.noreply.github.com>
2021-01-18 03:00:06 +0100

    Update Film.java

--- Random Projects/01KaartjesAutomaat/Film.java
+++ Random Projects/01KaartjesAutomaat/Film.java
@@ -1,26 +1,28 @@
-
 public class Film {
-	String naam;
-	double prijs;
-	
-	public Film(String naam, double prijs) {
-		this.naam = naam;
-		if (prijs < 5.00) {
-			this.prijs = 10.00;
-		} else if (prijs > 50.00) {
-			this.prijs = 50.00;
-		} else {
-			this.prijs = prijs;
-		}
-	}
+    String naam;
+    double prijs;
+    public static int aantalFilms;
 
-	public String getNaam() {
-		return naam;
-	}
+    public Film(String naam, double prijs) {
+        this.naam = naam;
+        if (prijs < 5.00) {
+            this.prijs = 10.00;
+        } else if (prijs > 50.00) {
+            this.prijs = 50.00;
+        } else {
+            this.prijs = prijs;
+        }
 
-	public double getPrijs() {
-		return prijs;
-	}
+        aantalFilms++;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
 
 
 }
