7541dbfcb92c83fcbc69373e053007573ead1115
Daniël <59751559+daniel1890@users.noreply.github.com>
2021-01-20 17:05:12 +0100

    Update Kaartje.java

--- Random Projects/01KaartjesAutomaat/Kaartje.java
+++ Random Projects/01KaartjesAutomaat/Kaartje.java
@@ -1,15 +1,29 @@
+import java.text.DecimalFormat;
+
 public class Kaartje {
     private String naam;
     private double prijs;
+    private DecimalFormat format;
     private static int kaartNummer = 0;
+    private double wisselgeld;
 
-    public Kaartje(String naam, double prijs) {
+    public Kaartje(String naam, double prijs, double wisselgeld, DecimalFormat format) {
         this.naam = naam;
         this.prijs = prijs;
+        this.wisselgeld = wisselgeld;
+        this.format = format;
         this.kaartNummer++;
     }
 
     public String printKaartjeConsole() {
-        return "Gekozen Film: " + naam + ",  prijs: €" + prijs + " , kaartnummer: " + kaartNummer;
+        String lijn1 = "Gekozen Film: " + naam + ",  prijs: €" + format.format(prijs);
+        String lijn2 = "";
+        if (wisselgeld >= 1.00) {
+            lijn2 += "Kaartnummer: " + kaartNummer + ", Uw wisselgeld: €" + format.format(wisselgeld);
+        } else if (wisselgeld < 1.00) {
+            lijn2 += "Kaartnummer: " + kaartNummer + ", Uw wisselgeld: €0" + format.format(wisselgeld);
+        }
+        return lijn1 + System.lineSeparator() + lijn2;
     }
+    
 }
